c973b84f20900a7dd3dbb0e2fe9fc1be9409ea35
jisooyun <jisooyun1994@gmail.com>
2016-10-11 16:39:39 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,14 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Tableau de bord</div>
+                <div class="panel-heading"> Liste de projets </div>
 
                 <div class="panel-body">
-
-                    <p> Liste de projets </p>
                     @foreach ($projets as $projet)
-                    <hr />
-                      {{$projet->name}}
-                      {{$projet->graph}}
-                      <a href="{{route('projets.show', $projet->id)}}">
-                        <button class="btn btn-info">
-                          Voir le projet
-                        </button>
-                      </a>
-                      <hr />
+
+                        <a href="{{route('projets.show', $projet->id)}}"> {{$projet->name}}</a>
+
+
                     @endforeach
                 </div>
             </div>
