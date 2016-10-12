78bc824bafe2f5303b8f8b27223f3d1aba108b4a
jisooyun <jisooyun1994@gmail.com>
2016-10-12 14:14:37 +0200

    modif style tableau de bord

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,29 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading"> Liste de projets </div>
+        <div class="col-md-6">
+            <div class="panel panel-success">
+                <div class="panel-heading "> Projets en cours </div>
 
                 <div class="panel-body">
-                  Projet en cours
-                  <br />
                     @foreach ($projets as $projet)
                      @if($projet->archive == 0)
 
                         <a href="{{route('projets.show', $projet->id)}}"> {{$projet->name}}</a> <br>
                       @endif
                     @endforeach
-                    <hr />
-                    Projet fini
-                    <br />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="panel panel-danger">
+                <div class="panel-heading"> Projets en cours </div>
+                <div class="panel-body">
                     @foreach ($projets as $projet)
-                     @if($projet->archive == 1)
-                        <a href="{{route('projets.show', $projet->id)}}"> {{$projet->name}}</a> <br>
-                      @endif
+                        @if($projet->archive == 1)
+                            <a href="{{route('projets.show', $projet->id)}}"> {{$projet->name}}</a>
+                            <br>
+                        @endif
                     @endforeach
                 </div>
             </div>
+
         </div>
     </div>
 </div>
